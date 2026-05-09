2571fc6e290b4e521f21402e93bbdc51126593a4
nazirjonshamshitdinov-pixel <nazirjonshamshitdinov@gmail.com>
2026-05-10 00:15:17 +0300

    Add files via upload

---
+++ vazifa3,3.py
@@ -0,0 +1,59 @@
+import os 
+os.system("cls")
+
+
+# 1- masala
+# n=int(input("son 1: "))
+# m=int(input("son 2: "))
+# list = []
+# for i in range(n,m,2):
+#     if n%2==0:
+#         list.append(i)
+#     else:
+#         list.append(i+1)
+# print(list)
+
+
+
+
+# 2 - masala
+# t = (1,2,3,4,5,6,7,8,9,10)
+# print(t[3],t[-4])
+
+
+
+
+# 3 -masala
+# l=[(10, 20, 40), (40, 50, 60), (70, 80, 90)]
+# l1 = []
+# for i in l:
+#     l1.append((i[0],i[1],100))
+# print(l1)
+
+
+
+
+
+# 4 -masala
+# matn=input("matn kiriting: ")
+# t = tuple(matn)
+# print(t)
+
+
+
+
+# 5 - masala
+# l = [ [1,2,3], [4,5,6], [10,11,12], [7,8,9] ]
+# m=max(l)
+# print(m)
+
+
+
+
+
+# 6 -masala 
+# list = [1,2,3,4]
+# string= "emp"
+# for i in range(len(list)):
+#     list[i]=string+str(list[i])
+# print(list)
